98599dbd99924b8b53e32b029c557e16cb300f1f
jobstefan773 <jspmegrino@addu.edu.ph>
2023-11-20 15:31:38 +0800

    update

--- DSA/GraphImplementation/Main.java
+++ DSA/GraphImplementation/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
         
         ArrayList<String> letters = new ArrayList<>();
         File file = new File("graph.txt");
